c8f656ea2c44fcaff2334cd6888983d7a0eebb2b
“Sam <scha4@gmu.edu>
2023-05-07 00:51:06 +0900

    aboutuspage

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,8 +1,38 @@
 export default function AboutUs() {
   return (
     <>
-      <div>
-        <h1>Who we are</h1>
+      <div className="AboutUs-page-container">
+        <h1>WHO WE ARE</h1>
+        <p>
+          {" "}
+          Welcome to NexusWorks! We specialize in product research, procurement,
+          and OEM/ODM production. With strong connections to factories based in
+          China, also known as the "World Factory," we have access to a vast
+          array of general and custom products, including electronic components,
+          household goods, injection-molded and metal products.
+        </p>
+        <p>
+          Our commitment to providing top-quality products at competitive prices
+          has earned us a reputation as a trusted partner for customers across
+          various industries, such as specialized factories, shopping malls,
+          open markets, manufacturing, wholesale, and distribution. We
+          prioritize the safety and satisfaction of our customers and aim to
+          provide fast and reliable service without any issues.
+        </p>
+        <p>
+          In addition to our extensive distribution networks in China, we offer
+          customized delivery solutions to meet your specific needs. Our team is
+          dedicated to delivering your products quickly and safely while
+          ensuring your complete satisfaction.
+        </p>
+        <p>
+          At NexusWorks, we take great pride in being a reliable trading partner
+          that you can trust. We are committed to providing exceptional service
+          and support throughout the entire product development and delivery
+          process. Thank you for considering NexusWorks for your product
+          sourcing and manufacturing needs. We look forward to the opportunity
+          to work with you.
+        </p>
       </div>
     </>
   );
